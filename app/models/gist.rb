ff2155b75f99476c2e73d4ec253c2ad085fac471
christophemilliere <christophemilliere93@gmail.com>
2015-08-25 15:02:36 +0200

    search by category

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -7,7 +7,11 @@ class Gist < ActiveRecord::Base
   belongs_to :category
 
   def self.search(search)
-    where("user LIKE ? OR name LIKE ? OR gists_id LIKE ?", "%#{search}%","%#{search}%","%#{search}%")
+    where("user LIKE ? OR name LIKE ? OR gists_id LIKE ? OR category_id LIKE ?", "%#{search}%","%#{search}%","%#{search}%","%#{self.search_category(search)}%")
+  end
+  def self.search_category(search)
+    cat = Category.find_by_name(search)
+    return cat.id if !cat.blank?
   end
 
 end
